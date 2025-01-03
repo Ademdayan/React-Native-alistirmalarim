2503308ae4f7dff54b13f66cdb13ba2088dda6ba
Adem Dayan <ademdayan36@gmail.com>
2025-01-04 02:20:18 +0300

    style verildi. button şekilendi.

--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -4,15 +4,45 @@ import PrimaryButton from '../components/PrimaryButton'
 
 const StartGamesScreen = () => {
   return (
-    <View>
+    <View style={styles.inputcontainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
-      
+
     </View>
   )
 }
 
 export default StartGamesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputcontainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 10,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 8,
+    },
+    input: {
+        height: 30,
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        marginVertical: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+    button: {
+        width: 100,
+    },
+})
