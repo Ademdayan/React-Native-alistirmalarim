61f079b0414c76c742ab8665a95e8ea0ea3c53a2
Adem Dayan <ademdayan36@gmail.com>
2025-01-10 18:25:51 +0300

    style verildi. button şekilendi. hem android hem ios için işlemler yapıldı. arka plan için resim eklendi.

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React from "react";
 
-export default function PrimaryButton({children}) {
+export default function PrimaryButton({ children }) {
+  function onPressHandler() {
+    console.log("Button pressed");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    elevation: 8,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: "#640233",
+  },
+});
--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -1,48 +1,68 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
-import React from 'react'
-import PrimaryButton from '../components/PrimaryButton'
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGamesScreen = () => {
   return (
     <View style={styles.inputcontainer}>
-      <TextInput />
+     <View style={styles.input}>
+     <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+     </View>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
-
     </View>
-  )
-}
+  );
+};
 
-export default StartGamesScreen
+export default StartGamesScreen;
 
 const styles = StyleSheet.create({
-    inputcontainer: {
-        padding: 16,
-        marginTop: 100,
-        marginHorizontal: 24,
-        borderRadius: 10,
-        backgroundColor: 'red',
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        elevation: 8,
-    },
-    input: {
-        height: 30,
-        borderBottomColor: 'grey',
-        borderBottomWidth: 1,
-        marginVertical: 10,
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15,
-    },
-    button: {
-        width: 100,
-    },
-})
+  inputcontainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 10,
+    backgroundColor: "red",
+   //alignItems: "center",
+   //justifyContent: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    
+  },
+  numberInput: {
+    height: 50,
+    width: 70,
+    fontSize: 32,
+    color: "black",
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    //backgroundColor: 'white',
+    marginVertical: 10,
+    textAlign: "center",
+    fontWeight: "bold",
+    textAlign: "center",
+    
+  },
+  input: {
+  alignItems: "center",
+  justifyContent: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+});
